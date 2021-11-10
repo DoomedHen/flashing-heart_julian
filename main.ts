9214259a2da65ee6e5c2fad90c4d08664e4fa7d2
DoomedHen <93943646+DoomedHen@users.noreply.github.com>
2021-11-10 13:55:56 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+function on_forever() {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+}
+
+basic.showNumber(500)
+basic.showIcon(IconNames.SmallHeart)
+basic.clearScreen()
+basic.pause(500)
+basic.showIcon(IconNames.Heart)
